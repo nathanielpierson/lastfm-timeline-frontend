1d3253eab83ed209a3375021cf0b67777ee3e217
nathanielpierson <nathanielepierson@gmail.com>
2025-06-26 14:35:34 -0400

    add picture and playcount returned

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,7 @@
 export function Header() {
   return (
     <div>
-      header
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Lastfm_logo.svg/2560px-Lastfm_logo.svg.png" height="35" width="120"/>
     </div>
   )
 }
--- src/UserTopAlbumsPage.jsx
+++ src/UserTopAlbumsPage.jsx
@@ -1,13 +1,36 @@
 import axios from "axios"
+import { useState, useEffect } from 'react'
+
 
 export function UserTopAlbumsPage() {
   const [albums, setAlbums] = useState([]);
-axios.get("http://localhost:3000/usertopalbums").then((response) => { 
-  setAlbums(response.data);
-})
+    const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const formData = new FormData(form);
+    const query = (form[0].value);
+    console.log(query);
+    axios.get("http://localhost:3000/usertopalbums", {
+      params: {
+        user: query
+      }
+    }).then((response) => { 
+      setAlbums(response.data);
+    })
+  }
+
   return (
     <div>
-
+      <form onSubmit={handleSubmit}>
+        <input name="query" />
+        <button type="submit">Search</button>
+      </form>
+      {albums.map((album) => (
+        <div key={album.id}>
+          {/* <img src={album.image[1]['#text']} /> */}
+          <h3>{album.name} <img src={album.image[1]['#text']} /> by {album.artist.name} {album.playcount} plays</h3>
+        </div>
+))};
     </div>
   )
 }
