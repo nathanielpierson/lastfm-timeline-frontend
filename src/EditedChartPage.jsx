a2f14a270e228b91ee674ae7ac761108b638ab99
nathanielpierson <nathanielepierson@gmail.com>
2025-07-25 16:04:24 -0400

    EditedChartPage shows all data points

--- src/EditedChartPage.jsx
+++ src/EditedChartPage.jsx
@@ -13,6 +13,25 @@ export function EditedChartPage() {
   useEffect (handleIndex, []);
   return (
 <div>
+  {topEditedAlbums.map((album) => 
+  (
+<div key={album.id}>
+  <h2>{album.title}</h2>
+  <p>{album.artist.name}</p>
+  {album.one_week != null ? <p> one week: {album.one_week} </p> : <p>one week: no data</p>
+}
+  {album.one_month != null ? <p> one month: {album.one_month} </p> : <p>one month: no data</p>
+}
+  {album.three_month != null ? <p> three month: {album.three_month} </p> : <p>three month: no data</p>
+}
+  {album.six_month != null ? <p> six month: {album.six_month} </p> : <p>six month: no data</p>
+}
+  {album.twelve_month != null ? <p> one year: {album.twelve_month} </p> : <p>one year: no data</p>
+}
+  {album.play_count_total != null ? <p> overall: {album.play_count_total} </p> : <p>overall: no data</p>
+}
+  </div>
+  ))}
   h
 </div>
   )
